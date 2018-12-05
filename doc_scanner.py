fd3a4434bf35136586378d767f1b2c55bcfb75db
Kevin de Youngster <kevin.deyoungster@gmail.com>
2018-12-05 14:09:31 +0000

    properly commented code

--- doc_scanner.py
+++ doc_scanner.py
@@ -17,10 +17,10 @@ def highlight_color(image, lower_color, upper_color):
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
     edged = cv2.Canny(mask, 75, 200)
-    # cv2.imshow('masked', edged)
+    # cv2.imshow("masked", edged)
     cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-    cv2.drawContours(image, [cnts[0]], -1, (0, 255, 0), 2)
+    cv2.drawContours(image, cnts, -1, (0, 255, 0), 2)
     # cv2.imshow("Final", image)
 
 
@@ -56,6 +56,17 @@ def get_quadrilateral_contour(edged_image):
         return None, False
 
 
+def get_contours(edged_image):
+    cnts = cv2.findContours(edged_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    if len(cnts) < 0:
+        print("No Contours Found in Image")
+        return None, False
+    else:
+        cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:10]
+        return cnts, True
+
+
 def getGrid(file_path):
 
     image = cv2.imread(file_path)
@@ -65,19 +76,29 @@ def getGrid(file_path):
     quadCnt, found = get_quadrilateral_contour(edged)
 
     if found:
+        # Draw contour of the identified quadrilateral
         cv2.drawContours(image, [quadCnt], -1, (0, 255, 0), 2)
-        cv2.imshow("Perspective", image)
+        cv2.imshow("Boundary Identified", image)
 
+        # Perform Perspective Transform to create top-down-view
         top_down_image = four_point_transform(original_image, quadCnt.reshape(4, 2))
-        image = top_down_image.copy()
+        cv2.imshow("Perspective Transform", top_down_image)
 
-        edged = get_edges(image)
-        # highlight_color(image, RED_LOWER, RED_UPPER)
-        # highlight_color(image, BLUE_LOWER, BLUE_UPPER)
+        # We'll be working with the top-down view henceforth, make that the main image
+        image = top_down_image
+
+        # # Draw edges of the image
+        # edged = get_edges(image, True)
+        # cnts, found = get_contours(edged)
+        # cv2.drawContours(image, cnts, 4, (0, 255, 0), 2)
+        # cv2.imshow("Edges", edged)
+
+        # Extract the colored objects (Start and Goal Positions)
+        highlight_color(image, RED_LOWER, RED_UPPER)
+        highlight_color(image, BLUE_LOWER, BLUE_UPPER)
+        cv2.imshow("Recognized Colors", image)
 
         cv2.imshow("Original Image", original_image)
-        cv2.imshow("Warped", top_down_image)
-
         cv2.waitKey(0)
         cv2.destroyAllWindows()
     else:
@@ -86,10 +107,7 @@ def getGrid(file_path):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    # detect_color(str(path), BLUE_LOWER,BLUE_UPPER)
-    # detect_color(str(path), RED_LOWER,RED_UPPER)
 
-
+getGrid("image2.JPG")
 # getGrid("file-3.jpeg")
-# getGrid("image2.JPG")
-getGrid("file5-1.jpeg")
+# getGrid("file5-1.jpeg")
